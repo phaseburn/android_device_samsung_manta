8ae8ac943bdf395dd8159a4f5e3c0c1f3eca9d7c
nocoast <voteforpedro8@gmail.com>
2013-02-02 15:36:35 -0600

    Added Morfic's Manta Patch ^_^

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,9 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/manta/bluetooth
 
 -include vendor/samsung/manta/BoardConfigVendor.mk
 
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_EXTRA_CFLAGS := -mtune=cortex-a15 -mcpu=cortex-a15
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
