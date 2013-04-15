9df0dc3331111a4fba8525b07e6e1ff6c0da335b
Matt Bloomfield <mbloomfield87@gmail.com>
2013-04-15 11:41:32 +0200

    added more languages + en_US as default
    making en_US as default will get rid of the Czech in quick settings on first boot

--- carbon.mk
+++ carbon.mk
@@ -22,7 +22,7 @@ PRODUCT_RELEASE_NAME := Nexus10
 $(call inherit-product, device/samsung/manta/full_manta.mk)
 
 # languages
-PRODUCT_LOCALES := cs_CZ da_DK de_AT de_CH de_DE de_LI el_GR en_AU en_CA en_GB en_NZ en_SG en_US es_ES fr_CA fr_CH fr_BE fr_FR it_CH it_IT ja_JP ka_GE ko_KR nb_NO nl_BE nl_NL nn_NO pl_PL pt_PT ru_RU sv_SE tr_TR zh_CN zh_HK zh_TW am_ET hi_IN
+PRODUCT_LOCALES := en_US fr_FR it_IT es_ES de_DE nl_NL cs_CZ pl_PL ja_JP zh_TW zh_CN ru_RU ko_KR nb_NO nn_NO es_US da_DK el_GR tr_TR pt_PT pt_BR rm_CH sv_SE bg_BG ca_ES en_GB fi_FI hi_IN hr_HR hu_HU in_ID iw_IL ka_GE lt_LT lv_LV ro_RO sk_SK sl_SI sr_RS uk_UA vi_VN tl_PH ar_EG fa_IR th_TH sw_TZ ms_MY af_ZA zu_ZA am_ET hi_IN
 
 # tablet
 $(call inherit-product, vendor/carbon/config/common_tablet.mk)
